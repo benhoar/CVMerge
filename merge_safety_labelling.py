0783ae04aef3f911c9625a1c35d260ed98cb5f58
JWandBH <112422778+JWandBH@users.noreply.github.com>
2022-10-23 12:33:37 -0700

    first merge commit

---
+++ merge_safety_labelling.py
@@ -0,0 +1,28 @@
+import pickle
+import pandas as pd, os
+path = './Samples' 
+for mech in os.listdir(path): 
+   files = os.listdir(f'{path}/{mech}')
+   files = [f for f in files if f.endswith('.txt')]
+   merge_safe = {}
+   conditional_safe = {}
+   for file in files:
+      df = pd.read_csv(f'{path}/{mech}/{file}')
+      df = df.loc[df['v'] == max(df['v'])]
+      n = len(df)//2
+      fwd = df.iloc[:n, :]
+      rev = df.iloc[n:, :]
+      d_fwd = abs(fwd.loc[fwd.index[-100], 'A'] - fwd.loc[fwd.index[-1], 'A'])
+      d_rev = abs(rev.loc[rev.index[-100], 'A'] - rev.loc[rev.index[-1], 'A'])
+      left_merge = d_rev <= 0.05
+      right_merge = d_fwd <= 0.05
+      if left_merge and right_merge:
+         merge_safe[file] = (left_merge, right_merge)
+      elif left_merge or right_merge:
+         conditional_safe[file] = (left_merge, right_merge)
+   pickle.dump(merge_safe, open(f'./Merge_Safety/{mech}_safe.pkl','wb'))
+   pickle.dump(conditional_safe, open(f'./Merge_Safety/{mech}_half_safe.pkl','wb'))
+   print(mech)
+   print(f'Safe: {len(merge_safe)}')
+   print(f'Half Safe: {len(conditional_safe)}')
+   print(f'Neither: {1-((len(merge_safe)+len(conditional_safe))/len(files))}')
